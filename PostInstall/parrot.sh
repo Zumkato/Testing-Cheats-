095236de4483cf2c7e704a83fe094dfd13987b93
ZumKato <zumkato@gmail.com>
2016-04-17 21:51:09 -0400

    Update parrot.sh
    new xfce

--- PostInstall/parrot.sh
+++ PostInstall/parrot.sh
@@ -537,31 +537,34 @@ else
   echo -e "\n\n ${YELLOW}[i]${RESET} ${YELLOW}Skipping GNOME${RESET}..." 1>&2
 fi
 
+
 ##### Install XFCE4
-echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}XFCE4${RESET}${RESET} ~ desktop environment"
-export DISPLAY=:0.0   #[[ -z $SSH_CONNECTION ]] || export DISPLAY=:0.0
-apt-get -y -qq install curl || echo -e ' '${RED}'[!] Issue with apt-get'${RESET} 1>&2
-apt-get -y -qq install xfce4 xfce4-places-plugin || echo -e ' '${RED}'[!] Issue with apt-get'${RESET}    #xfce4-goodies   xfce4-battery-plugin  xfce4-mount-plugin
-#apt-get -y -qq install shiki-colors-xfwm-theme     # theme from repos
+(( STAGE++ )); echo -e "\n\n ${GREEN}[+]${RESET} (${STAGE}/${TOTAL}) Installing ${GREEN}XFCE4${RESET}${RESET} ~ desktop environment"
+export DISPLAY=:0.0
+apt -y -qq install curl \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+apt -y -qq install xfce4 xfce4-mount-plugin xfce4-notifyd xfce4-places-plugin \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+(dmidecode | grep -iq virtual) \
+  || (apt -y -qq install xfce4-battery-plugin \
+    || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2)
 #--- Configuring XFCE
-mv -f /usr/bin/startx{,-gnome}
-ln -sf /usr/bin/startx{fce4,}
-mkdir -p ~/.config/xfce4/{desktop,menu,panel,xfconf,xfwm4}/
-mkdir -p ~/.config/xfce4/panel/launcher-{2,4,5,6,8,9}/
+mkdir -p ~/.config/xfce4/panel/launcher-{2,4,5,6,7,8,9}/
 mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
-cat <<EOF > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
+cat <<EOF > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml \
+  || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
 <?xml version="1.0" encoding="UTF-8"?>
 <channel name="xfce4-keyboard-shortcuts" version="1.0">
   <property name="commands" type="empty">
     <property name="custom" type="empty">
       <property name="XF86Display" type="string" value="xfce4-display-settings --minimal"/>
       <property name="&lt;Alt&gt;F2" type="string" value="xfrun4"/>
+      <property name="&lt;Primary&gt;space" type="string" value="xfce4-appfinder"/>
       <property name="&lt;Primary&gt;&lt;Alt&gt;t" type="string" value="/usr/bin/exo-open --launch TerminalEmulator"/>
       <property name="&lt;Primary&gt;&lt;Alt&gt;Delete" type="string" value="xflock4"/>
       <property name="&lt;Primary&gt;Escape" type="string" value="xfdesktop --menu"/>
       <property name="&lt;Super&gt;p" type="string" value="xfce4-display-settings --minimal"/>
       <property name="override" type="bool" value="true"/>
-      <property name="&lt;Primary&gt;space" type="string" value="xfce4-appfinder"/>
     </property>
   </property>
   <property name="xfwm4" type="empty">
@@ -630,140 +633,95 @@ cat <<EOF > ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.
   </property>
 </channel>
 EOF
-cat <<EOF > ~/.config/xfce4/panel/launcher-2/13684522758.desktop || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
-[Desktop Entry]
-Name=Terminal Emulator
-Encoding=UTF-8
-Exec=exo-open --launch TerminalEmulator
-Icon=utilities-terminal
-StartupNotify=false
-Terminal=false
-Comment=Use the command line
-Type=Application
-Categories=Utility;X-XFCE;X-Xfce-Toplevel;
-X-XFCE-Source=file:///usr/share/applications/exo-terminal-emulator.desktop
-EOF
-cat <<EOF > ~/.config/xfce4/panel/launcher-6/13684522587.desktop || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
-[Desktop Entry]
-Name=Iceweasel
-Encoding=UTF-8
-Exec=iceweasel %u
-Icon=iceweasel
-StartupNotify=true
-Terminal=false
-Comment=Browse the World Wide Web
-GenericName=Web Browser
-X-GNOME-FullName=Iceweasel Web Browser
-X-MultipleArgs=false
-Type=Application
-Categories=Network;WebBrowser;
-MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
-StartupWMClass=Iceweasel
-X-XFCE-Source=file:///usr/share/applications/iceweasel.desktop
-EOF
-cat <<EOF > ~/.config/xfce4/panel/launcher-8/13684522859.desktop || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
-[Desktop Entry]
-Name=Geany
-Encoding=UTF-8
-Exec=geany %F
-Icon=geany
-StartupNotify=true
-Terminal=false
-Comment=A fast and lightweight IDE using GTK2
-GenericName=Integrated Development Environment
-Type=Application
-Categories=GTK;Development;IDE;
-MimeType=text/plain;text/x-chdr;text/x-csrc;text/x-c++hdr;text/x-c++src;text/x-java;text/x-dsrc;text/x-pascal;text/x-perl;text/x-python;application/x-php;application/x-httpd-php3;application/x-httpd-php4;application/x-httpd-php5;application/xml;text/html;text/css;text/x-sql;text/x-diff;
-X-XFCE-Source=file:///usr/share/applications/geany.desktop
-EOF
-cat <<EOF > ~/.config/xfce4/panel/launcher-9/136845425410.desktop || echo -e ' '${RED}'[!] Issue with writing file'${RESET} 1>&2
-[Desktop Entry]
-Name=Application Finder
-Exec=xfce4-appfinder
-Icon=xfce4-appfinder
-StartupNotify=true
-Terminal=false
-Type=Application
-Categories=X-XFCE;Utility;
-Comment=Find and launch applications installed on your system
-X-XFCE-Source=file:///usr/share/applications/xfce4-appfinder.desktop
-EOF
+#--- Desktop files
+ln -sf /usr/share/applications/exo-terminal-emulator.desktop ~/.config/xfce4/panel/launcher-2/exo-terminal-emulator.desktop
+ln -sf /usr/share/applications/kali-wireshark.desktop        ~/.config/xfce4/panel/launcher-4/kali-wireshark.desktop
+ln -sf /usr/share/applications/firefox-esr.desktop           ~/.config/xfce4/panel/launcher-5/firefox-esr.desktop
+ln -sf /usr/share/applications/kali-burpsuite.desktop        ~/.config/xfce4/panel/launcher-6/kali-burpsuite.desktop
+ln -sf /usr/share/applications/kali-msfconsole.desktop       ~/.config/xfce4/panel/launcher-7/kali-msfconsole.desktop
+ln -sf /usr/share/applications/org.gnome.gedit.desktop       ~/.config/xfce4/panel/launcher-8/textedit.desktop
+ln -sf /usr/share/applications/xfce4-appfinder.desktop       ~/.config/xfce4/panel/launcher-9/xfce4-appfinder.desktop
+#--- XFCE settings
 _TMP=""
-[ "${burpFree}" != "false" ] && _TMP="-t int -s 5"
+[ "${burpFree}" != "false" ] \
+  && _TMP="-t int -s 6"
 xfconf-query -n -a -c xfce4-panel -p /panels -t int -s 0
 xfconf-query --create --channel xfce4-panel --property /panels/panel-0/plugin-ids \
-  -t int -s 1  -t int -s 2  -t int -s 3  -t int -s 4  ${_TMP}  -t int -s 6  -t int -s 8  -t int -s 9 \
+  -t int -s 1   -t int -s 2   -t int -s 3   -t int -s 4   -t int -s 5  ${_TMP}        -t int -s 7   -t int -s 8  -t int -s 9 \
   -t int -s 10  -t int -s 11  -t int -s 13  -t int -s 15  -t int -s 16  -t int -s 17  -t int -s 19  -t int -s 20
 xfconf-query -n -c xfce4-panel -p /panels/panel-0/length -t int -s 100
 xfconf-query -n -c xfce4-panel -p /panels/panel-0/size -t int -s 30
 xfconf-query -n -c xfce4-panel -p /panels/panel-0/position -t string -s "p=6;x=0;y=0"
 xfconf-query -n -c xfce4-panel -p /panels/panel-0/position-locked -t bool -s true
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-1 -t string -s applicationsmenu  # application menu
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-2 -t string -s launcher          # terminal  ID: 13684522758
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-3 -t string -s places            # places
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-4 -t string -s launcher          # wireshark ID: 14470234761
-[ "${burpFree}" != "false" ] && xfconf-query -n -c xfce4-panel -p /plugins/plugin-5 -t string -s launcher     # burpsuite ID: 14470234962
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-6 -t string -s launcher          # iceweasel ID: 13684522587
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-8 -t string -s launcher          # geany     ID: 13684522859  (geany gets installed later)
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-9 -t string -s launcher          # search    ID: 136845425410
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-1 -t string -s applicationsmenu     # application menu
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-2 -t string -s launcher             # terminal   ID: exo-terminal-emulator
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-3 -t string -s places               # places
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-4 -t string -s launcher             # wireshark  ID: kali-wireshark
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-5 -t string -s launcher             # firefox    ID: firefox-esr
+[ "${burpFree}" != "false" ] \
+  && xfconf-query -n -c xfce4-panel -p /plugins/plugin-6 -t string -s launcher        # burpsuite  ID: kali-burpsuite
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-7 -t string -s launcher             # msf        ID: kali-msfconsole
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-8 -t string -s launcher             # gedit      ID: org.gnome.gedit.desktop
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-9 -t string -s launcher             # search     ID: xfce4-appfinder
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-10 -t string -s tasklist
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-11 -t string -s separator
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-13 -t string -s mixer            # audio
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-13 -t string -s mixer   # audio
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-15 -t string -s systray
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-16 -t string -s actions
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-17 -t string -s clock
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-19 -t string -s pager
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-20 -t string -s showdesktop
-# application menu
+#--- application menu
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/show-tooltips -t bool -s true
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/show-button-title -t bool -s false
-# terminal
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-2/items -t string -s "13684522758.desktop" -a
-# places
+#--- terminal
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-2/items -t string -s "exo-terminal-emulator.desktop" -a
+#--- places
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/mount-open-volumes -t bool -s true
-# wireshark
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/items -t string -s "14470234761.desktop" -a
-# burp
-[ "${burpFree}" != "false" ] && xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/items -t string -s "14470234962.desktop" -a
-# iceweasel
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-6/items -t string -s "13684522587.desktop" -a
-# geany
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-8/items -t string -s "13684522859.desktop" -a
-# search
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-9/items -t string -s "136845425410.desktop" -a
-# tasklist (& separator - required for padding)
+#--- wireshark
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/items -t string -s "kali-wireshark.desktop" -a
+#--- firefox
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/items -t string -s "firefox-esr.desktop" -a
+#--- burp
+[ "${burpFree}" != "false" ] \
+  && xfconf-query -n -c xfce4-panel -p /plugins/plugin-6/items -t string -s "kali-burpsuite.desktop" -a
+#--- metasploit
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-7/items -t string -s "kali-msfconsole.desktop" -a
+#--- gedit/atom
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-8/items -t string -s "textedit.desktop" -a
+#--- search
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-9/items -t string -s "xfce4-appfinder.desktop" -a
+#--- tasklist (& separator - required for padding)
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-10/show-labels -t bool -s true
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-10/show-handle -t bool -s false
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-11/style -t int -s 0
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-11/expand -t bool -s true
-# systray
+#--- systray
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-15/show-frame -t bool -s false
-# actions
+#--- actions
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-16/appearance -t int -s 1
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-16/items -t string -s "+logout-dialog" -t string -s "-switch-user" -t string -s "-separator" -t string -s "-logout" -t string -s "+lock-screen" -t string -s "+hibernate" -t string -s "+suspend" -t string -s "+restart" -t string -s "+shutdown" -a
-# clock
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-16/items \
+  -t string -s "+logout-dialog"  -t string -s "-switch-user"  -t string -s "-separator" \
+  -t string -s "-logout"  -t string -s "+lock-screen"  -t string -s "+hibernate"  -t string -s "+suspend"  -t string -s "+restart"  -t string -s "+shutdown"  -a
+#--- clock
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-17/show-frame -t bool -s false
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-17/mode -t int -s 2
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-17/digital-format -t string -s "%R, %Y-%m-%d"
-# pager / workspace
+#--- pager / workspace
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-19/miniature-view -t bool -s true
 xfconf-query -n -c xfce4-panel -p /plugins/plugin-19/rows -t int -s 1
-xfconf-query -n -c xfwm4 -p /general/workspace_count -t int -s 5
+xfconf-query -n -c xfwm4 -p /general/workspace_count -t int -s 3
 #--- Theme options
 xfconf-query -n -c xsettings -p /Net/ThemeName -s "Kali-X"
 xfconf-query -n -c xsettings -p /Net/IconThemeName -s "Vibrancy-Kali"
 xfconf-query -n -c xsettings -p /Gtk/MenuImages -t bool -s true
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/button-icon -t string -s "parrot-menu"
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/button-icon -t string -s "kali-menu"
 #--- Window management
 xfconf-query -n -c xfwm4 -p /general/snap_to_border -t bool -s true
 xfconf-query -n -c xfwm4 -p /general/snap_to_windows -t bool -s true
 xfconf-query -n -c xfwm4 -p /general/wrap_windows -t bool -s false
 xfconf-query -n -c xfwm4 -p /general/wrap_workspaces -t bool -s false
 xfconf-query -n -c xfwm4 -p /general/click_to_focus -t bool -s false
-#--- TouchPad
-#xfconf-query -n -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Edge_Scrolling -t int -s 0 -t int -s 0 -t int -s 0
-#xfconf-query -n -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Tap_Action -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0
-#xfconf-query -n -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Two-Finger_Scrolling -t int -s 1 -t int -s 1
 xfconf-query -n -c xfwm4 -p /general/click_to_focus -t bool -s true
 #--- Hide icons
 xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -t bool -s false
@@ -774,20 +732,55 @@ xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-removable -t 
 xfconf-query -n -c xfce4-session -p /splash/Engine -t string -s ""
 xfconf-query -n -c xfce4-session -p /shutdown/LockScreen -t bool -s true
 xfconf-query -n -c xfce4-session -p /general/SaveOnExit -t bool -s false
-#--- Power options
-#xfconf-query -n -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-ac -t int -s 1
-#xfconf-query -n -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-battery -t int -s 1
 #--- App Finder
 xfconf-query -n -c xfce4-appfinder -p /last/pane-position -t int -s 248
 xfconf-query -n -c xfce4-appfinder -p /last/window-height -t int -s 742
 xfconf-query -n -c xfce4-appfinder -p /last/window-width -t int -s 648
-#--- Remove Mail Reader from menu
-file=/usr/share/applications/exo-mail-reader.desktop   #; [ -e "${file}" ] && cp -n $file{,.bkup}
-sed -i 's/^NotShowIn=*/NotShowIn=XFCE;/; s/^OnlyShowIn=XFCE;/OnlyShowIn=/' "${file}"
-grep -q "NotShowIn=XFCE" "${file}" || echo "NotShowIn=XFCE;" >> "${file}"
 #--- Enable compositing
 xfconf-query -n -c xfwm4 -p /general/use_compositing -t bool -s true
 xfconf-query -n -c xfwm4 -p /general/frame_opacity -t int -s 85
+#--- Remove "Mail Reader" from menu
+file=/usr/share/applications/exo-mail-reader.desktop   #; [ -e "${file}" ] && cp -n $file{,.bkup}
+sed -i 's/^NotShowIn=*/NotShowIn=XFCE;/; s/^OnlyShowIn=XFCE;/OnlyShowIn=/' "${file}"
+grep -q "NotShowIn=XFCE" "${file}" \
+  || echo "NotShowIn=XFCE;" >> "${file}"
+#--- XFCE for default applications
+mkdir -p ~/.local/share/applications/
+file=~/.local/share/applications/mimeapps.list; [ -e "${file}" ] && cp -n $file{,.bkup}
+[ ! -e "${file}" ] \
+  && echo '[Added Associations]' > "${file}"
+([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+#--- Firefox
+for VALUE in http https; do
+  sed -i 's#^x-scheme-handler/'${VALUE}'=.*#x-scheme-handler/'${VALUE}'=exo-web-browser.desktop#' "${file}"
+  grep -q '^x-scheme-handler/'${VALUE}'=' "${file}" 2>/dev/null \
+    || echo 'x-scheme-handler/'${VALUE}'=exo-web-browser.desktop' >> "${file}"
+done
+#--- Thunar
+for VALUE in file trash; do
+  sed -i 's#x-scheme-handler/'${VALUE}'=.*#x-scheme-handler/'${VALUE}'=exo-file-manager.desktop#' "${file}"
+  grep -q '^x-scheme-handler/'${VALUE}'=' "${file}" 2>/dev/null \
+    || echo 'x-scheme-handler/'${VALUE}'=exo-file-manager.desktop' >> "${file}"
+done
+file=~/.config/xfce4/helpers.rc; [ -e "${file}" ] && cp -n $file{,.bkup}
+([[ -e "${file}" && "$(tail -c 1 ${file})" != "" ]]) && echo >> "${file}"
+sed -i 's#^FileManager=.*#FileManager=Thunar#' "${file}" 2>/dev/null
+grep -q '^FileManager=Thunar' "${file}" 2>/dev/null \
+  || echo 'FileManager=Thunar' >> "${file}"
+#--- Disable user folders in home folder
+file=/etc/xdg/user-dirs.conf; [ -e "${file}" ] && cp -n $file{,.bkup}
+sed -i 's/^XDG_/#XDG_/g; s/^#XDG_DESKTOP/XDG_DESKTOP/g;' "${file}"
+sed -i 's/^enable=.*/enable=False/' "${file}"
+find ~/ -maxdepth 1 -mindepth 1 -type d \
+  \( -name 'Documents' -o -name 'Music' -o -name 'Pictures' -o -name 'Public' -o -name 'Templates' -o -name 'Videos' \) -empty -delete
+apt -y -qq install xdg-user-dirs \
+  || echo -e ' '${RED}'[!] Issue with apt install'${RESET} 1>&2
+xdg-user-dirs-update
+#--- Remove any old sessions
+rm -f ~/.cache/sessions/*
+#--- Set XFCE as default desktop manager
+update-alternatives --set x-session-manager /usr/bin/xfce4-session   #update-alternatives --config x-window-manager   #echo "xfce4-session" > ~/.xsession
+
 
 ##### Configure XFCE4
 echo -e "\n ${GREEN}[+]${RESET} Configuring ${GREEN}XFCE4${RESET}${RESET} ~ desktop environment"
